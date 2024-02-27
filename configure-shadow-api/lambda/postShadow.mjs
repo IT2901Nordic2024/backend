fb6247176674ab708f2d767ba138c3a8dc6dec08
EskildVeren <eskild.overen@gmail.com>
2024-02-27 13:20:32 +0100

    [#15] feat: Add GET method in lamdba function

--- configure-shadow-api/lambda/postShadow.mjs
+++ configure-shadow-api/lambda/postShadow.mjs
@@ -1,5 +1,20 @@
-
-
 export const handler = async (event, context) => {
+    // Initiating response to sender
+    let body;
+    let statusCode = 200;
+    const headers = {
+        "Content-Type": "application/json",
+    };
+    const url = "https://a2aclgd4nh1dkk-ats.iot.eu-north-1.amazonaws.com/things/firmwareSimulatorThing/shadow?name=demo_shadow_delete_later"
     
+    let response = await fetch(url);
+    let data = await response.json();
+    return data;
+
+    return {
+        body,
+        statusCode,
+        headers
+    }
+
 }
