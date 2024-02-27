dce49b1d4c6275fb02136250f78854091287e33e
EskildVeren <eskild.overen@gmail.com>
2024-02-27 13:21:47 +0100

    [#15] feat: Add account and region to stack environment

--- configure-shadow-api/bin/configure-shadow-api.ts
+++ configure-shadow-api/bin/configure-shadow-api.ts
@@ -5,6 +5,10 @@ import { ConfigureShadowApiStack } from '../lib/configure-shadow-api-stack';
 
 const app = new cdk.App();
 new ConfigureShadowApiStack(app, 'ConfigureShadowApiStack', {
+  env: { 
+    account: process.env.CDK_DEFAULT_ACCOUNT, 
+    region: process.env.CDK_DEFAULT_REGION ,
+}
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
