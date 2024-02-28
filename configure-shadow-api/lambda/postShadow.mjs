cc6a5df728c78bda4452fe905a35425beb02e527
EskildVeren <eskild.overen@gmail.com>
2024-02-28 12:45:38 +0100

    [#15] feat: Create handler for getting shadow

--- configure-shadow-api/lambda/postShadow.mjs
+++ configure-shadow-api/lambda/postShadow.mjs
@@ -1,20 +1,24 @@
+import { IoTDataPlaneClient, GetThingShadowCommand,  } from "@aws-sdk/client-iot-data-plane"; // ES Modules import
+
 export const handler = async (event, context) => {
     // Initiating response to sender
-    let body;
-    let statusCode = 200;
-    const headers = {
-        "Content-Type": "application/json",
-    };
-    const url = "https://a2aclgd4nh1dkk-ats.iot.eu-north-1.amazonaws.com/things/firmwareSimulatorThing/shadow?name=demo_shadow_delete_later"
-    
-    let response = await fetch(url);
-    let data = await response.json();
-    return data;
-
-    return {
-        body,
-        statusCode,
-        headers
+    const client = new IoTDataPlaneClient({
+        region: "eu-north-1",
+        logger: console,
+        endpoint: "https://a2aclgd4nh1dkk-ats.iot.eu-north-1.amazonaws.com"
+    });
+    let response = ""
+    try {
+        const command = new GetThingShadowCommand({
+            thingName: "firmwareSimulatorThing",
+            //shadowName: "shadow?name=demo_shadow_delete_later"   
+            //shadow?name=demo_shadow_delete_later
+        });
+        response = await client.send(command);
+    }
+    catch (err){
+        response = err.message
     }
 
+    return response
 }
