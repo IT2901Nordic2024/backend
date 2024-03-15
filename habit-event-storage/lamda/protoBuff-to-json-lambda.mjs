4a52b9a95703d925e9c67f0682a46212f41c6514
Mythois <hanneswitt@gmail.com>
2024-03-15 14:43:53 +0100

    [#21]feat: Initialise habit-event-storage cdk app

---
+++ habit-event-storage/lamda/protoBuff-to-json-lambda.mjs
@@ -0,0 +1,7 @@
+import * as protobuf from 'protobufjs'
+
+const PROTO_FILE = 'habit-event-storage\\proto-files\\fromFirmwareToBackend.proto'
+
+export const handler = async (event) => {
+  console.log(event)
+}
