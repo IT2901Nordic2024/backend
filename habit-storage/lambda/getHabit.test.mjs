084e9640ca21740d86b48a35b8e688d343bba629
EskildVeren <eskild.overen@gmail.com>
2024-03-20 11:43:23 +0100

    [#34] test: Clean up code in getHabit.test.mjs

--- habit-storage/lambda/getHabit.test.mjs
+++ habit-storage/lambda/getHabit.test.mjs
@@ -1,23 +1,7 @@
-/* eslint-disable */
-
-//import { handler } from '../lambda/getHabits.mjs'
 import { mockClient } from 'aws-sdk-client-mock'
-// const { mockClient } = require('aws-sdk-client-mock')
-// const { DynamoDBDocumentClient, GetCommand } = require('@aws-sdk/lib-dynamodb')
-// const { describe } = require('node:test')
-//const { handler } = require('./getHabits.mjs')
 import { handler } from './getHabits.mjs'
-//import * as m from './getHabits.mjs'
-//import handler = require('./getHabits.mjs')
-
 import { DynamoDBDocumentClient, GetCommand } from '@aws-sdk/lib-dynamodb'
-
-//const ddbMock = mockClient(DynamoDBDocumentClient)
-//const tableName = process.env.HABIT_TABLE_NAME
-
-function add(a, b) {
-  return a + b
-}
+import { beforeEach, describe, it, expect } from '@jest/globals'
 
 const ddbMock = mockClient(DynamoDBDocumentClient)
 
