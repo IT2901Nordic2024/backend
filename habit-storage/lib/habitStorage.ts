210c79f3bfc440dd17da6ddbf5fc3016f3a6803d
EskildVeren <eskild.overen@gmail.com>
2024-04-10 15:47:32 +0200

    [#51] feat: Created resources for lambda function and API routes

--- habit-storage/lib/habitStorage.ts
+++ habit-storage/lib/habitStorage.ts
@@ -14,6 +14,7 @@ export class HabitStorage extends Construct {
   public readonly getHabitsWithSideHandler: lambda.Function
   public readonly createHabitHandler: lambda.Function
   public readonly editHabitHandler: lambda.Function
+  public readonly deleteHabitHandler: lambda.Function
 
   constructor(scope: Construct, id: string) {
     super(scope, id)
@@ -43,15 +44,6 @@ export class HabitStorage extends Construct {
       partitionKey: { name: 'userId', type: dynamodb.AttributeType.NUMBER },
     })
 
-    const indexName: dynamodb.GlobalSecondaryIndexProps = {
-      // REMOVE NOW
-      indexName: 'indexName',
-      partitionKey: {
-        name: 'deviceId',
-        type: dynamodb.AttributeType.STRING,
-      },
-    }
-
     const deviceIdIndex: dynamodb.GlobalSecondaryIndexProps = {
       indexName: 'deviceIdIndex',
       partitionKey: {
@@ -60,7 +52,6 @@ export class HabitStorage extends Construct {
       },
     }
 
-    userDataTable.addGlobalSecondaryIndex(indexName) // REMOVE NOW
     userDataTable.addGlobalSecondaryIndex(deviceIdIndex)
 
     // Handler for accessing DynamoDB table
@@ -112,17 +103,32 @@ export class HabitStorage extends Construct {
       },
     })
 
+    const deleteHabitHandler = new lambda.Function(this, 'DeleteHabitHandler', {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'deleteHabit.handler',
+      functionName: 'DeleteHabitHandler',
+      role: createHabitHandlerRole,
+
+      environment: {
+        USER_DATA_TABLENAME: userDataTable.tableName,
+        HABIT_EVENT_TABLENAME: 'HabitEventTable',
+      },
+    })
+
     // Setting the table and handler for this construct
     this.table = userDataTable
     this.getHabitsHandler = getHabitsHandler
     this.createHabitHandler = createHabitHandler
     this.getHabitsWithSideHandler = getHabitWithSide
     this.editHabitHandler = editHabitHandler
+    this.deleteHabitHandler = deleteHabitHandler
 
     // Granting handler read and write access to the table
     userDataTable.grantReadWriteData(this.getHabitsHandler)
     userDataTable.grantReadWriteData(this.createHabitHandler)
     userDataTable.grantReadWriteData(this.editHabitHandler)
     userDataTable.grantReadData(this.getHabitsWithSideHandler)
+    userDataTable.grantReadWriteData(this.deleteHabitHandler)
   }
 }
