407696a0d20629c08e6073e6f39a68638f8fb231
Mythois <hanneswitt@gmail.com>
2024-04-12 15:24:44 +0200

    [#48]feat: Add storing capability to storingHabitDataLambda.mjs

--- habit-event-storage/lambda/storingHabitDataLambda.mjs
+++ habit-event-storage/lambda/storingHabitDataLambda.mjs
@@ -12,10 +12,15 @@ import { DynamoDB, DynamoDBClient } from '@aws-sdk/client-dynamodb'
 import { PutCommand, DynamoDBDocumentClient, UpdateCommand, QueryCommand } from '@aws-sdk/lib-dynamodb'
 
 export const handler = async (event, context) => {
-  console.log('Event:', event)
-  console.log('payload: ', event.payload)
-  console.log('deviceId: ', event.deviceId)
+  // Defining working variables
+  let user
+  let userID
+  let habit
+  let habitType
+  let newPayload = []
+  const habitID = event.payload.habitId
 
+  // Table names
   const habitEventTableName = process.env.HABIT_EVENT_TABLENAME
   const userDataTable = process.env.USER_DATA_TABLENAME
 
@@ -29,26 +34,52 @@ export const handler = async (event, context) => {
    */
 
   /**
-   * Get user ID
+   * Get userData
    *
    *
    * Sources:
    * https://www.youtube.com/watch?v=94kLlixSxfY
    * https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/GettingStarted.ReadItem.html
    * */
-  const getUserID = new QueryCommand({
-    TableName: userDataTable,
-    IndexName: 'deviceIdIndex',
-    KeyConditionExpression: 'deviceId = :deviceId',
-    ExpressionAttributeValues: {
-      ':deviceId': event.deviceId,
-    },
-  })
+  try {
+    const getUserData = new QueryCommand({
+      TableName: userDataTable,
+      IndexName: 'deviceIdIndex',
+      KeyConditionExpression: 'deviceId = :deviceId',
+      ExpressionAttributeValues: {
+        ':deviceId': event.deviceId,
+      },
+    })
 
-  const user = await docClient.send(getUserID)
-  const userID = user.Items[0].userId
-  console.log('userID:', userID)
-  console.log('userID type: ', typeof userID)
+    user = await docClient.send(getUserData)
+    userID = user.Items[0].userId
+    habit = user.Items[0].habits.filter((habit) => {
+      return habit.habitId === habitID
+    })[0]
+    habitType = habit.habitType
+  } catch (error) {
+    console.log('Error: ', error)
+  }
+
+  /**
+   * Create the payload to be saved to the database, based on event.payload
+   */
+
+  // Remove habit_id from payload before storing it in the database, because it should not be stored under habitEvents
+  delete event.payload.habitId
+  switch (habitType) {
+    case 'COUNT':
+      newPayload = [event.payload.deviceTimestamp, event.payload.data]
+      console.log('COUNT')
+      break
+
+    case 'TIME':
+      newPayload = [event.payload.deviceTimestamp, event.payload.startTimestamp, event.payload.stopTimestamp]
+      console.log('TIME')
+      break
+    default:
+      throw new Error(`No habit of this type ${habitType} of this kind`)
+  }
 
   /**
    * Update habit event data
@@ -59,22 +90,20 @@ export const handler = async (event, context) => {
    * https://how.wtf/dynamodb-error-key-element-does-not-match-the-schema.html
    *
    *   */
-
   const updateHabitEvent = new UpdateCommand({
     TableName: habitEventTableName,
     Key: {
       userId: userID,
-      habitId: event.payload.habitId,
+      habitId: habitID,
     },
     UpdateExpression: 'SET habitEvents = list_append(habitEvents,:habitEvent)',
     ExpressionAttributeValues: {
-      ':habitEvent': [delete event.payload.habitId], // Remove habit_id from payload before storing it in the database, because it should not be stored under habitEvents
+      ':habitEvent': [newPayload],
     },
   })
 
   const response = await docClient.send(updateHabitEvent)
   console.log('response:', response)
 
-  console.log('Hello World')
   return event
 }
