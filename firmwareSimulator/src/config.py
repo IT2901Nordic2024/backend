84c3ffba56e0243fdf5b142d77bd5735394db0c2
Mythois <hanneswitt@gmail.com>
2024-04-25 15:45:04 +0200

    [#69]fix: Update paths in config.py
    The paths are updated to create the certificates in the correct folders.

--- firmwareSimulator/src/config.py
+++ firmwareSimulator/src/config.py
@@ -9,18 +9,17 @@
 #AWS_ENDPOINT = 'a2aclgd4nh1dkk-ats.iot.eu-north-1.amazonaws.com'
 #PUB_KEY_FILE_PATH = '../firmwareSimulator/certificates2/FirmwareSimulatorThing.public.key'
 
-CERT_FILE_PATH = "../firmwareSimulator/certificates/firmwareSimulatorThing.cert.pem"
-PRI_KEY_FILE_PATH = "../firmwareSimulator/certificates/firmwareSimulatorThing.private.key"
-ROOT_PEM_FILE_PATH = "../firmwareSimulator/certificates/root.pem"
+CERT_FILE_PATH = "../FirmwareSimulator/certificates/FirmwareSimulatorThing.cert.pem"
+PRI_KEY_FILE_PATH = "../FirmwareSimulator/certificates/FirmwareSimulatorThing.private.key"
+ROOT_PEM_FILE_PATH = "../FirmwareSimulator/certificates/root.pem"
 AWS_ENDPOINT = "a2aclgd4nh1dkk-ats.iot.eu-north-1.amazonaws.com"
-PUB_KEY_FILE_PATH = '../firmwareSimulator/certificates/firmwareSimulatorThing.public.key'
-PATH_TO_DODECAHEDRON = '../firmwareSimulator/dodecahedron/dodecahedron.json'
+PUB_KEY_FILE_PATH = '../FirmwareSimulator/certificates/FirmwareSimulatorThing.public.key'
+PATH_TO_DODECAHEDRON = '../FirmwareSimulator/dodecahedron/dodecahedron.json'
 #---------------------------------------------------------------------------
 #                                  Constants                  
 #---------------------------------------------------------------------------
 
-#THING_NAME = CLIENT_ID = 'FirmwareSimulatorThing'                           # Using two variables for readability, because aws sometimes uses client_id and sometimes thingName.
-THING_NAME = CLIENT_ID = 'firmwareSimulatorThing'
+THING_NAME = CLIENT_ID = 'FirmwareSimulatorThing'                           # Using two variables for readability, because aws sometimes uses client_id and sometimes thingName.
 POLICY_NAME = THING_NAME + 'Policy'
 PUBLISH_MQTT_TOPIC = 'habit-tracker-data/' + THING_NAME + '/events'
 REGION_NAME = 'eu-north-1'
